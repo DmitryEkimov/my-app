fc4e7339de3213dbeba99ca80226fb7917a6f3d6
Dmitry Ekimov <dmitry_ekimov@hotmail.com>
2021-02-07 17:18:45 +0700

    Update index.js

--- src/routes/Game/index.js
+++ src/routes/Game/index.js
@@ -4,9 +4,9 @@ import PokemonCard from '../../components/PokemonCard';
 import s from './style.module.css'
 import POKEMONS from '../../assets/pokemons.json'
 const GamePage = ()=> {
-    const [pokemonCards,setPokemonCards] = useState(POKEMONS);
-    const setActive =(id)=>{
-        setPokemonCards(prevState=>{
+    const [pokemons,setPokemons] = useState(POKEMONS);
+    const handleChangeActive =(id)=>{
+        setPokemons(prevState=>{
             const pokemonCards = prevState.map(item=>
                 (item.id===id ? { ...item, active: !item.active } : item));
             return pokemonCards; });
@@ -15,7 +15,7 @@ const GamePage = ()=> {
         <Layout id="cards" title="Cards" colorTitle="#FEFEFE" colorBg="#202736">
             <div className={s.flex}>
                 {
-                    pokemonCards.map(item=><PokemonCard name={item.name} img={item.img} id={item.id} type={item.type} key={item.id} values={item.values} isActive={item.active} setActive={setActive} />)
+                    pokemons.map(item=><PokemonCard name={item.name} img={item.img} id={item.id} type={item.type} key={item.id} values={item.values} isActive={item.active} onClickCard={handleChangeActive} />)
                 }
             </div>
         </Layout>
